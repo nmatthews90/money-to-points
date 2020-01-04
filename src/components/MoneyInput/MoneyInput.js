4ff1feead7cef3156408afcc3db971b7c37c8060
Natasha Matthews <Natasha@MacBook-Pro.local>
2020-01-04 11:52:56 -0600

    added the ablilty to input values

---
+++ src/components/MoneyInput/MoneyInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MoneyInput = (props) => {
+    return (
+        <div>
+            {props.purchaseAmount}
+        </div>
+    )
+}
+
+export default MoneyInput;

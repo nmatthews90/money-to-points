05fbf09e80b4fb81f6b70ff07969994e3e7ff876
Natasha Matthews <Natasha@MacBook-Pro.local>
2020-01-04 10:49:13 -0600

    Took out some create-react-app styles.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <p>A customer receives 2 points for every dollar spent over $100 in each transaction, plus 1 point for every dollar spent over $50 in each transaction.</p>
+        <p>(e.g. a $120 purchase = 2x$20 + 1x$50 = 90 points).</p>
     </div>
   );
 }

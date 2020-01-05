29494cfb3118d76192b4ab377787523690782a8c
Natasha Matthews <Natasha@MacBook-Pro.local>
2020-01-05 10:12:51 -0600

    Updates

---
+++ precache-manifest.d58c8eebb54dc99b1920d2090e14f97d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b698d53ee55e1383cdb8df3a9497d43e",
+    "url": "/money-to-points/index.html"
+  },
+  {
+    "revision": "18cd500b9cb2a86a08c9",
+    "url": "/money-to-points/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "834bb51495fd75ce2d6f",
+    "url": "/money-to-points/static/css/main.ccd63339.chunk.css"
+  },
+  {
+    "revision": "18cd500b9cb2a86a08c9",
+    "url": "/money-to-points/static/js/2.0c526e5e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/money-to-points/static/js/2.0c526e5e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "834bb51495fd75ce2d6f",
+    "url": "/money-to-points/static/js/main.d7261494.chunk.js"
+  },
+  {
+    "revision": "1cd6e7790b65180d1c6c",
+    "url": "/money-to-points/static/js/runtime-main.b33326ff.js"
+  }
+]);
